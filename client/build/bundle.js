2cccff6a83c5c2b876e84e3f0323b151fd4974c3
Jay Chetty <jaypaulchetty@gmail.com>
2016-05-21 15:48:40 +0100

    remove unused reducers

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -37682,22 +37682,11 @@
 
 	'use strict';
 	
-	// function allScared(chickens){
-	//   _.every(catachableChickens, (chicken)=>{
-	//     return chicken.scare < 1
-	//   })
-	// }
-	
 	function approachDispatcher(store) {
 	  store.dispatch({
 	    type: 'NEXT_APPROACH',
 	    catcher: 1
 	  });
-	  // if allScared(store.getState().chickens.items){
-	  //   store.dispatch({
-	  //     type:'FINISH_APPROACH'
-	  //   })
-	  // }
 	}
 	
 	module.exports = approachDispatcher;
--- client/src/action_dispatchers/approach_dispatcher.js
+++ client/src/action_dispatchers/approach_dispatcher.js
@@ -1,19 +1,8 @@
-// function allScared(chickens){
-//   _.every(catachableChickens, (chicken)=>{
-//     return chicken.scare < 1
-//   })
-// }
-
 function approachDispatcher(store){
   store.dispatch({
     type:'NEXT_APPROACH',
     catcher:1
   })
-  // if allScared(store.getState().chickens.items){
-  //   store.dispatch({
-  //     type:'FINISH_APPROACH'
-  //   })
-  // }
 }
 
 module.exports = approachDispatcher
